de0b60446fc70fd01a0967a7601197fa85461c6a
Asmaaelkashif <asmaaelkashef9@gmail.com>
2024-11-10 00:50:16 +0200

    first commit

---
+++ BMI calculater/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:project_name/bmiResult.dart';
+import 'package:project_name/bmiScreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    //title: 'Flutter Counter',
+    home: Bmiscreen(  ),
+  ));
+}
